Fixing repulsion and repulsion derivative tests

tests/test_nuclear_deriv.py
@@ -75,8 +75,7 @@ class TestNuclearDeriv(unittest.TestCase):
                 for k in range(0,3):  # loop over nuclei
                     for l in range(0,3):  # loop over directions
                         force = integrator.nuclear_deriv(cgfs[i], cgfs[j], O, Ochg, nuclei[k][0], l)
-                        np.testing.assert_almost_equal(force, vals[i,j,k,l], 4)
-                        
+                        np.testing.assert_almost_equal(force, vals[i,j,k,l], 4)            
 
 def calculate_force_finite_difference(molfile, basisfile, 
                                       nuc_id, cgf_id1, cgf_id2, coord):

tests/test_repulsion.py
@@ -7,6 +7,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from ppmil import Molecule, PPMIL, GTO
+import pyqint
 
 class TestRepulsion(unittest.TestCase):
 
@@ -71,12 +72,12 @@ class TestRepulsion(unittest.TestCase):
 
         # build hydrogen molecule
         mol = Molecule("H2O")
-        mol.add_atom('H', 0.00000, -0.07579, 0.00000)
+        mol.add_atom('O', 0.00000, -0.07579, 0.00000)
         mol.add_atom('H', 0.86681, 0.60144, 0.00000)
         mol.add_atom('H',  -0.86681, 0.60144, 0.00000)
         fname = os.path.join(os.path.dirname(__file__), 'data', 'sto3g.json')
         cgfs, nuclei = mol.build_basis('sto3g', fname)
-                
+        
         N = len(cgfs)
         fname = os.path.join(os.path.dirname(__file__), 'data', 'repulsion_h2o.txt')
         vals = np.loadtxt(fname).reshape((N,N,N,N))

tests/test_repulsion_deriv.py
@@ -69,7 +69,7 @@ class TestRepulsionDerivatives(unittest.TestCase):
         
         # build hydrogen molecule
         mol = Molecule("H2O")
-        mol.add_atom('H', 0.00000, -0.07579, 0.00000)
+        mol.add_atom('O', 0.00000, -0.07579, 0.00000)
         mol.add_atom('H', 0.86681, 0.60144, 0.00000)
         mol.add_atom('H',  -0.86681, 0.60144, 0.00000)
         basisfile = os.path.join(os.path.dirname(__file__), 'data', 'sto3g.json')
@@ -86,7 +86,6 @@ class TestRepulsionDerivatives(unittest.TestCase):
                             for n in range(0,3):  # loop over directions
                                 force = integrator.repulsion_deriv(cgfs[i], cgfs[j], cgfs[k], cgfs[l], nuclei[m][0], n)
                                 np.testing.assert_almost_equal(force, vals[i,j,k,l,m,n], 4)
-                        
 
 def calculate_force_finite_difference(molfile, basisfile, 
                                       cgf_id1, cgf_id2, cgf_id3, cgf_id4, 