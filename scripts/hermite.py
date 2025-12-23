3ca4e94e7cf6879384a898b8f3cfd48553a6d29a
ifilot <ivo@ivofilot.nl>
2025-12-23 09:48:45 +0100

    Adding script to study hermite polynomials

---
+++ scripts/hermite.py
@@ -0,0 +1,18 @@
+from scipy.special import hermite, factorial
+import numpy as np
+import matplotlib.pyplot as plt
+
+def hermite_exp(x,l):
+    res = np.zeros_like(x)
+    pre = 2**(-l)
+    for i in range(l//2+1):
+        H = hermite(l - 2*i)
+        res += factorial(l) / factorial(i) / factorial (l - 2*i) * H(x)
+
+    return pre * res
+
+l = 6
+x = np.linspace(-1, 1, 50)
+plt.plot(x, x**l)
+plt.plot(x, hermite_exp(x, l), 'o', alpha=0.5)
+plt.show()
